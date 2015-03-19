951cdb77b03439b4e98276e85b9be731112ac311
Jonas Almeida <jalmeida@mathbiol.org>
2015-03-19 13:55:05 -0400

    load URL compositions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# localphi
-local, in-browser object to manage personal health information (PHI) in a manner that is expeditious and safe
+# LocalPHI
+###Your Personal Health Information stays put unless you say otherwise.
+<i>local, in-browser object to manage personal health information (PHI) in a manner that is expeditious and safe</i>
+
+
--- localphi.js
+++ localphi.js
@@ -2,3 +2,14 @@ console.log('localphi.js loaded')
 
 // lets try to use HL7 FHIR data model as closely as possibly to make hings easier later if a backed is engage
 
+localphi=function(x){
+    this.x=x
+    // ---- ini ---
+    // look for a URL to load if this is localphi.html
+    var m = document.location.href.match(/localphi.html\?(http:\/\/.*)/)
+    if(m.length==2){$.getScript(m[1])}
+}
+
+// start
+new localphi
+
